48e4ad3d8a3c70a5d3ad85ccfc530553a367316b
Akseli Epailys <akseli.epailys@hotmail.com>
2023-10-06 14:40:16 +0300

    delete unnecessary stuff

--- EnvironmentalDataLogger/src/main/java/fi/tuni/environmentaldatalogger/EnvironmentalDataLogger.java
+++ EnvironmentalDataLogger/src/main/java/fi/tuni/environmentaldatalogger/EnvironmentalDataLogger.java
@@ -10,11 +10,7 @@ import java.io.IOException;
 public class EnvironmentalDataLogger extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(EnvironmentalDataLogger.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+
     }
 
     public static void main(String[] args) {
