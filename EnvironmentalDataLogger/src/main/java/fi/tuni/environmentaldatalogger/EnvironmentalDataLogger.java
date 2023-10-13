42b8aac7a3a66dbe2b4bfc1cfdecad0291b50176
Miro Anttila <miro.anttila@tuni.fi>
2023-10-13 13:11:30 +0300

    first linechart

--- EnvironmentalDataLogger/src/main/java/fi/tuni/environmentaldatalogger/EnvironmentalDataLogger.java
+++ EnvironmentalDataLogger/src/main/java/fi/tuni/environmentaldatalogger/EnvironmentalDataLogger.java
@@ -3,23 +3,34 @@ package fi.tuni.environmentaldatalogger;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import javafx.util.Pair;
-import java.util.Date;
-import java.util.Map;
-import java.util.TreeMap;
 
+import java.util.*;
 
 
 public class EnvironmentalDataLogger extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(EnvironmentalDataLogger.class.getResource("gui_template.fxml"));
+
+
+        Presenter presenter = new Presenter();
+        ArrayList<String> params = new ArrayList<>(Collections.singletonList("temp"));
+        Date startDate = new Date(2023-1900, Calendar.OCTOBER, 23);
+        Date endDate = new Date(2023-1900, Calendar.NOVEMBER, 10);
+        System.out.println(startDate);
+        Pair<Date, Date> range = new Pair<Date, Date>(startDate, endDate);
+        LineChart<String, Number> lineChart = presenter.getDataAsLineChart(params, range);
+        Scene scene1 = new Scene(lineChart);
+
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setTitle("Environmental Data Logger");
-        stage.setScene(scene);
+
+        stage.setScene(scene1);
         stage.show();
 
         DataExtractor extractor = WeatherDataExtractor.getInstance();
@@ -27,12 +38,13 @@ public class EnvironmentalDataLogger extends Application {
 
         System.out.println("Valid parameters: " + extractor.getValidParameters());
 
+
         // Get valid data range for a parameter
         //Pair<Date, Date> range = extractor.getValidDataRange("temperature");
         //System.out.println("Valid data range for temperature: " + range.getKey() + " to " + range.getValue());
         Coordinate coordinates = new Coordinate(0, 0);
-        Date startDate = new Date(); // replace with actual date
-        Date endDate = new Date(); // replace with actual date
+        //Date startDate = new Date(); // replace with actual date
+        //Date endDate = new Date(); // replace with actual date
         TreeMap<Date, Double> data = extractor.getData("temp", new Pair<>(startDate, endDate), coordinates);
 
         // Print the data
