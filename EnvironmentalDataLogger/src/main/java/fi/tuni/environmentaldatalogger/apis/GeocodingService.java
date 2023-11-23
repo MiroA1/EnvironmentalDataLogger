6d851290a4fe317ec9d4dca41dd9c6fe4e4df0e1
ville <vllhtnn@gmail.com>
2023-11-23 11:57:05 +0200

    javaDoc for GeocodingService and WeatherDataExtractor

--- EnvironmentalDataLogger/src/main/java/fi/tuni/environmentaldatalogger/apis/GeocodingService.java
+++ EnvironmentalDataLogger/src/main/java/fi/tuni/environmentaldatalogger/apis/GeocodingService.java
@@ -6,16 +6,30 @@ import com.opencagedata.jopencage.model.JOpenCageLatLng;
 import com.opencagedata.jopencage.model.JOpenCageResponse;
 import fi.tuni.environmentaldatalogger.util.Coordinate;
 
+/**
+ * The GeocodingService class provides geocoding services by interacting with the JOpenCage Geocoding API.
+ * It converts a location query (like an address) into geographical coordinates (latitude and longitude).
+ */
 public class GeocodingService {
 
     private static final String API_KEY = "5409ecbc4e384bddb83f0461b2234d9d";
     private static GeocodingService instance;
     private final JOpenCageGeocoder geocoder;
 
+    /**
+     * Private constructor for GeocodingService.
+     * Initializes the JOpenCageGeocoder with the API key.
+     */
     private GeocodingService() {
         this.geocoder = new JOpenCageGeocoder(API_KEY);
     }
 
+    /**
+     * Gets the singleton instance of GeocodingService.
+     * If the instance does not exist, it is created.
+     *
+     * @return The single instance of GeocodingService.
+     */
     public static synchronized GeocodingService getInstance() {
         if (instance == null) {
             instance = new GeocodingService();
@@ -23,6 +37,14 @@ public class GeocodingService {
         return instance;
     }
 
+    /**
+     * Converts a location query string into geographical coordinates.
+     * Only the first result from the geocoding service is returned.
+     *
+     * @param query The location query string, like an address or place name.
+     * @return A Coordinate object containing the latitude and longitude of the location,
+     *         or null if no result is found or in case of an error.
+     */
     public Coordinate getCoordinates(String query) {
         JOpenCageForwardRequest request = new JOpenCageForwardRequest(query);
         request.setLimit(1); // get only first result
@@ -37,4 +59,5 @@ public class GeocodingService {
         }
     }
 }
+
 
